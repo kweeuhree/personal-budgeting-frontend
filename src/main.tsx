19db7bb74ff68643b5fd5d61b5ae133ab0fb516e
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-22 10:10:40 -0500

    feat: import store into the main point of entry and pass store to Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')!).render(
+import { store } from "./store/store.ts";
+
+import App from "./App.tsx";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </StrictMode>
+);
