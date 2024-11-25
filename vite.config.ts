f9d0b4374248f9a6ceeac343379dcc66fd2b74bd
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-25 13:05:18 -0500

    fix: ensure proper tailwind installation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    https: {
+      key: "./tls/key.pem",
+      cert: "./tls/cert.pem",
+    },
+    proxy: {
+      "/api": {
+        target: "https://localhost:4000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  css: {
+    postcss: "./postcss.config.js",
+  },
+});
