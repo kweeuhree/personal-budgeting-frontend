84fca67a9febb778173266c8839f20e3446de625
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-25 15:58:33 -0500

    feat: add a log out button to DefaultLayout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,10 +1,28 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 
+import { useAppDispatch, useLogoutMutation, userLogout } from "../store";
+
 export const DefaultLayout: React.FC = () => {
+  const [logout] = useLogoutMutation();
+  const dispatch = useAppDispatch();
+
+  const handleLogout = () => {
+    try {
+      logout({});
+      dispatch(userLogout());
+    } catch (error) {
+      throw new Error(error ? error.toString() : "Failed to log out the user");
+    }
+  };
+
   return (
     <div>
-      <div>Navigation Bar</div>
+      <div>
+        Navigation Bar
+        <button type="button" onClick={handleLogout}>
+          Log out
+        </button>
+      </div>
       <main>
         <Outlet />
       </main>
