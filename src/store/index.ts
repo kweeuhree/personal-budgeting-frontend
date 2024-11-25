61014535537fd9665baf76ca10e140661b3dde58
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-25 09:53:01 -0500

    feat: update store exports

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,3 +2,4 @@ export * from "./store";
 export * from "./actions";
 export * from "./slices";
 export * from "./middleware";
+export * from "./hooks";
--- src/store/slices/index.ts
+++ src/store/slices/index.ts
@@ -2,3 +2,4 @@ export * from "./budgetSlice";
 export * from "./expenseCategorySlice";
 export * from "./expensesSlice";
 export * from "./userSlice";
+export * from "./csrfSlice";
