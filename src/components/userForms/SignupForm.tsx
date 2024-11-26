75b7fe4448c5fa8ac84cfd2fd289adafaf553649
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-26 10:11:14 -0500

    feat: full stack sign up functionality

--- src/components/userForms/SignupForm.tsx
+++ src/components/userForms/SignupForm.tsx
@@ -1,24 +1,43 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { UserSignUp } from "../../types";
+import { useSignupMutation } from "../../store";
 
 export const SignupForm: React.FC = () => {
-  const { reset } = useForm<UserSignUp>();
+  const [signup, { isSuccess, isError, error }] = useSignupMutation();
 
   const {
     register,
     handleSubmit,
     watch,
+    reset,
     formState: { errors },
   } = useForm<UserSignUp>();
-  const onSubmit: SubmitHandler<UserSignUp> = (data) => {
-    console.log(data);
-    reset();
+  const onSubmit: SubmitHandler<UserSignUp> = (userData) => {
+    console.log(userData);
+    handleSignup(userData);
   };
 
   const password = watch("password");
 
+  const handleSignup = async (userData: UserSignUp) => {
+    try {
+      await signup({
+        email: userData.userEmail,
+        displayName: userData.displayName,
+        password: userData.password,
+      }).unwrap();
+      reset();
+    } catch (error) {
+      throw new Error("Failed to sign up");
+    }
+  };
+
   return (
     <form id="signupForm" onSubmit={handleSubmit(onSubmit)}>
+      {isSuccess && <p className="success-message">Signed up successfully!</p>}
+      {isError && error && (
+        <p className="error-message">This email address is already is use</p>
+      )}
       <label htmlFor="displayName">Display name:</label>
       <input
         id="displayName"
@@ -27,6 +46,7 @@ export const SignupForm: React.FC = () => {
           required: "Display name is required",
         })}
       />
+      <br />
       {errors.displayName && <span>{errors.displayName.message}</span>}
       <br />
       <label htmlFor="email">Email:</label>
@@ -41,6 +61,7 @@ export const SignupForm: React.FC = () => {
           },
         })}
       />
+      <br />
       {errors.userEmail && <span>{errors.userEmail.message}</span>}
       <br />
       <label htmlFor="password">Password:</label>
@@ -57,17 +78,20 @@ export const SignupForm: React.FC = () => {
       />
       <br />
       {errors.password && <span>This field is required</span>}
+      <br />
       <label htmlFor="repeatPassword">Repeat password:</label>
       <input
         id="repeatPassword"
         type="password"
-        {...register("password", {
+        {...register("repeatPassword", {
           required: "Password is required",
           validate: (value) => value === password || "Passwords do not match",
         })}
       />
       <br />
       {errors.repeatPassword && <span>{errors.repeatPassword.message}</span>}
+      <br />
+      <button type="submit">Sign up</button>
     </form>
   );
 };
