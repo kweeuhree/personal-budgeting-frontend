1734a3bced0b63a7fff32bef6e14c34b387237ac
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-29 13:31:42 -0500

    feat: add a preliminary BudgetPage

--- src/pages/BudgetPage.tsx
+++ src/pages/BudgetPage.tsx
@@ -1,3 +1,25 @@
+import React from "react";
+import { selectBudget, useAppSelector } from "../store";
+import { formatBudget } from "../utils";
+
 export const BudgetPage = () => {
-  return <div>BudgetPage</div>;
+  const budget = useAppSelector(selectBudget);
+  const formattedBudget = formatBudget(budget);
+  const {
+    checkingBalance,
+    savingsBalance,
+    budgetTotal,
+    totalSpent,
+    budgetRemaining,
+  } = formattedBudget;
+
+  return (
+    <div>
+      <div>Checking balance: {checkingBalance}</div>
+      <div>Savings balance: {savingsBalance}</div>
+      <div>Total budget: {budgetTotal}</div>
+      <div>Total spent: {totalSpent}</div>
+      <div>Total remaining: {budgetRemaining}</div>
+    </div>
+  );
 };
