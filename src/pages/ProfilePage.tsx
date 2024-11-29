57d113fb8552f9430dc6523f71a0721376528d87
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-29 13:32:00 -0500

    feat: add a preliminary ProfilePage

---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  budgetDelete,
+  selectBudget,
+  selectUser,
+  useAppDispatch,
+  useAppSelector,
+  useDeleteBudgetMutation,
+} from "../store";
+
+export const ProfilePage = () => {
+  const dispatch = useAppDispatch();
+  const { displayName } = useAppSelector(selectUser);
+  const [deleteBudget, { error, isSuccess }] = useDeleteBudgetMutation();
+  const { budgetId } = useAppSelector(selectBudget);
+
+  const greeting = `Hello, ${displayName || "friend"}!`;
+
+  const handleDeleteBudget = () => {
+    const confirmStmt =
+      "This action cannot be undone. Are you sure that you want to reset budget?";
+    if (confirm(confirmStmt)) {
+      try {
+        deleteBudget(budgetId);
+        dispatch(budgetDelete());
+      } catch (error) {
+        throw new Error("Failed deleting budget");
+      }
+    }
+    return;
+  };
+
+  return (
+    <div>
+      <div>{error ? "there was an error" : isSuccess && "budget reset"}</div>
+      <div>{greeting}</div>
+      {budgetId ? (
+        <button onClick={handleDeleteBudget}>Reset budget</button>
+      ) : (
+        "create a budget to start tracking expenses"
+      )}
+    </div>
+  );
+};
