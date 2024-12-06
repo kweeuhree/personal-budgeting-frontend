79ad69924a64b4d46e66f2fedbc74e98a4501c74
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-06 10:19:29 -0500

    feat: implement listenerMiddleware that is listening for user and category endpoints

--- src/store/middleware.ts
+++ src/store/middleware.ts
@@ -3,6 +3,7 @@ import { createLogger } from "redux-logger";
 
 import { categoryApi, expenseApi, authApi } from "./apis";
 import { addCategories, addExpenses } from "./slices";
+import { resetStoreState } from "./actions";
 
 export const logger = createLogger({
   collapsed: true, // Collapse actions for better readability
@@ -12,7 +13,7 @@ export const logger = createLogger({
 });
 
 export const listenerMiddleware = createListenerMiddleware();
-
+// Listen for a successful login
 listenerMiddleware.startListening({
   matcher: authApi.endpoints.login.matchFulfilled,
   effect: async (_, { dispatch }) => {
@@ -20,13 +21,49 @@ listenerMiddleware.startListening({
       const categories = await dispatch(
         categoryApi.endpoints.fetchAllCategories.initiate({})
       ).unwrap();
+      if (categories.length === 0) {
+        return;
+      }
+      dispatch(addCategories(categories));
       const expenses = await dispatch(
         expenseApi.endpoints.fetchAllExpenses.initiate({})
       ).unwrap();
-      if (categories && Object.keys(categories).length !== 0)
-        dispatch(addCategories(categories));
-      if (expenses && Object.keys(expenses).length !== 0)
-        dispatch(addExpenses(expenses));
+
+      if (expenses.length !== 0) dispatch(addExpenses(expenses));
+    } catch (error) {
+      throw new Error(
+        `Failed to fetch additional user information: ${error instanceof Error ? error.message : "Unknown error."}`
+      );
+    }
+  },
+});
+
+// Listen for a successful logout
+listenerMiddleware.startListening({
+  matcher: authApi.endpoints.logout.matchFulfilled,
+  effect: (_, { dispatch }) => {
+    try {
+      dispatch(resetStoreState());
+    } catch (error) {
+      throw new Error(
+        `Failed to fetch additional user information: ${error instanceof Error ? error.message : "Unknown error."}`
+      );
+    }
+  },
+});
+
+// Listen for a successful category creation
+listenerMiddleware.startListening({
+  matcher: categoryApi.endpoints.categoryCreate.matchFulfilled,
+  effect: async (_, { dispatch }) => {
+    try {
+      const categories = await dispatch(
+        categoryApi.endpoints.fetchAllCategories.initiate({})
+      ).unwrap();
+      if (categories.length === 0) {
+        return;
+      }
+      dispatch(addCategories(categories));
     } catch (error) {
       throw new Error(
         `Failed to fetch additional user information: ${error instanceof Error ? error.message : "Unknown error."}`
