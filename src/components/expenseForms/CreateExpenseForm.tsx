331c25c9d2d3c74d2773cc86940b460ae4b6eeb9
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-06 10:16:20 -0500

    feat: update CreateExpenseForm component - add a header; - remove redundant name input and its label; - correctly implement CategoriesDropdown; - implement AccountFieldset; - add expenseType input within a fieldset tag.

--- src/components/expenseForms/CreateExpenseForm.tsx
+++ src/components/expenseForms/CreateExpenseForm.tsx
@@ -1,26 +1,30 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 import {
   selectCategories,
   useAppSelector,
-  useCreateExpenseMutation,
-  // createExpense,
+  useExpenseCreateMutation,
+  useCategoryCreateMutation,
+  useAppDispatch,
+  budgetUpdate,
 } from "../../store";
 import { type StringInput } from "../../types";
 import { AccountFieldset, CategoriesDropdown } from "..";
+import { getCategoryId, convertStringtoCents } from "../../utils";
 
 export const CreateExpenseForm: React.FC = () => {
   // should send a post request to the database
   // should check whether any category exists,
   // should send a post request to category model before creating
   // the expense
+  const [expenseCreate, { isSuccess, error }] = useExpenseCreateMutation();
+  const [categoryCreate] = useCategoryCreateMutation();
   const categories = useAppSelector(selectCategories);
   const categoriesExist = categories.length > 0;
-  console.log(`Categories? - ${categoriesExist}`);
-  const [createExpense, { isSuccess, error }] = useCreateExpenseMutation();
+
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
   const {
     // reset,
@@ -33,8 +37,36 @@ export const CreateExpenseForm: React.FC = () => {
     handleCreateExpense(expenseData);
   };
 
-  const handleCreateExpense = (expenseData: StringInput) => {
+  const handleCreateExpense = async (expenseData: StringInput) => {
     console.log(`Attemting to create expense: ${expenseData}`);
+    const amountInCents = convertStringtoCents(expenseData.value);
+    const categoryId = getCategoryId(categories, expenseData.category);
+    console.log(categoryId);
+    try {
+      if (categoriesExist) {
+        const updatedBudget = await expenseCreate({
+          description: expenseData.descripton,
+          category: expenseData.category,
+          expenseType: expenseData.expenseType,
+          amountInCents,
+          categoryId,
+        }).unwrap();
+        console.log("new expense", updatedBudget);
+        dispatch(budgetUpdate(updatedBudget));
+      } else {
+        const newCategory = await categoryCreate({
+          name: expenseData.category,
+        }).unwrap();
+        const newExpense = await expenseCreate(expenseData).unwrap();
+        console.log("new expense", newExpense);
+        console.log("new category", newCategory);
+      }
+
+      handleNavigateToBudget();
+    } catch (error) {
+      console.error("Error occurred while creating expense:", error);
+      throw new Error(error instanceof Error ? error.message : "Unknown error");
+    }
   };
 
   const handleNavigateToBudget = () => {
@@ -44,21 +76,8 @@ export const CreateExpenseForm: React.FC = () => {
   return (
     <>
       {isSuccess ? "Expense created" : error && "error"}
+      <h1>Create Expense</h1>
       <form id="createExpenseForm" onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          type="text"
-          {...register("name", {
-            required: "Name is required",
-            min: {
-              value: 0,
-              message: "Please include a name",
-            },
-          })}
-        />
-        {errors.name && <span>This field is required</span>}
-        <br />
         <label htmlFor="description">Description</label>
         <input id="description" type="text" {...register("description", {})} />
         {errors.description && <span>{errors.description.toString()}</span>}
@@ -76,7 +95,11 @@ export const CreateExpenseForm: React.FC = () => {
         />
         <br />
         {categoriesExist ? (
-          <CategoriesDropdown categories={categories} />
+          <CategoriesDropdown
+            register={register}
+            errors={errors}
+            categories={categories}
+          />
         ) : (
           <>
             <label htmlFor="category">Category</label>
@@ -93,7 +116,36 @@ export const CreateExpenseForm: React.FC = () => {
             />
           </>
         )}
-        <AccountFieldset register={register} errors={errors} />
+        <AccountFieldset
+          register={register}
+          errors={errors}
+          name={"expenseType"}
+        />
+        {/* <fieldset>
+          <legend>Account</legend>
+
+          <input
+            id="CheckingBalance"
+            type="radio"
+            value="CheckingBalance"
+            {...register("expenseType", {
+              required: "Select an account type",
+            })}
+          />
+          <label htmlFor="CheckingBalance">Checking</label>
+
+          <input
+            id="SavingsBalance"
+            type="radio"
+            value="SavingsBalance"
+            {...register("expenseType", {
+              required: "Select an account type",
+            })}
+          />
+          <label htmlFor="SavingsBalance">Savings</label>
+
+          {errors.balanceType && <span>{errors.balanceType.message}</span>}
+        </fieldset> */}
         <button type="button" onClick={handleNavigateToBudget}>
           Cancel
         </button>
