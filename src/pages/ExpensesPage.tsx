2e6bf0dcae75a736ba8549e61c8c404bba9a6d67
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-06 10:17:32 -0500

    feat: add expense deletion logic

--- src/pages/ExpensesPage.tsx
+++ src/pages/ExpensesPage.tsx
@@ -1,8 +1,51 @@
-import React from "react";
+import {
+  selectAllExpenses,
+  useAppDispatch,
+  useAppSelector,
+  useExpenseDeleteMutation,
+  deleteExpense,
+  budgetUpdate,
+} from "../store";
+import { Expenses } from "../components";
 
 export const ExpensesPage = () => {
-  // should fetch expenses and update state if they exist
-  // should display a message if no expenses exist
-  // should dipslay an add category form if no category exists
-  return <div>ExpensesPage</div>;
+  const expenses = useAppSelector(selectAllExpenses);
+  const [expenseDelete, { isSuccess, error }] = useExpenseDeleteMutation();
+  const dispatch = useAppDispatch();
+
+  const handleDelete = async (expenseId: string) => {
+    try {
+      console.log("attempting delete...");
+      const updatedBudget = await expenseDelete(expenseId).unwrap();
+      dispatch(deleteExpense(expenseId));
+      dispatch(budgetUpdate(updatedBudget));
+    } catch (error) {
+      throw new Error(
+        `Failed to delete expense: ${error instanceof Error ? error.message : error}`
+      );
+    }
+  };
+
+  const handleConfirmDelete = (expenseId: string, amountInDollars: string) => {
+    const found = expenses.find((exp) => exp.expenseId === expenseId);
+    const confirmStmt = `Are you sure you want to permanently delete ${amountInDollars}?`;
+    if (found && confirm(confirmStmt)) {
+      handleDelete(expenseId);
+    }
+  };
+
+  return (
+    <>
+      <div>ExpensesPage</div>
+      {isSuccess ? "Expense deleted" : error && "Error"}
+      {expenses.length > 0 ? (
+        <Expenses
+          handleConfirmDelete={handleConfirmDelete}
+          expenses={expenses}
+        />
+      ) : (
+        "no expenses"
+      )}
+    </>
+  );
 };
