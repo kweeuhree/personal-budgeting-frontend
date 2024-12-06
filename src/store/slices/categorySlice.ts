a7df68f9fc408245ad70562d46d3369ed6003c1d
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-06 10:20:04 -0500

    fix: correct categoryId to be expenseCategoryId

--- src/store/slices/categorySlice.ts
+++ src/store/slices/categorySlice.ts
@@ -17,7 +17,9 @@ const categorySlice = createAppSlice({
       state.push(action.payload);
     },
     deleteCategory: (state: Categories, action: PayloadAction<Category>) => {
-      state.filter((cat) => cat.categoryId !== action.payload.categoryId);
+      state.filter(
+        (cat) => cat.expenseCategoryId !== action.payload.expenseCategoryId
+      );
     },
   },
   extraReducers: (builder) => {
@@ -26,7 +28,7 @@ const categorySlice = createAppSlice({
   selectors: {
     selectCategories: (state: Categories): Categories => state,
     selectSpecificCategory: (state: Categories, categoryId: string) => {
-      return state.find((cat) => cat.categoryId === categoryId);
+      return state.find((cat) => cat.expenseCategoryId === categoryId);
     },
   },
 });
