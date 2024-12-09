26e48949527675fd355e27a17f0e8bee8816c7f6
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-09 14:09:30 -0500

    feat: display categories as a table instead of a list

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -10,26 +10,36 @@ export const Categories: React.FC<Props> = ({
   categories,
   handleConfirmDelete,
 }) => {
-  const categoriesJSX = categories.map((cat) => {
-    const { expenseCategoryId, name, description, totalSum } = cat;
-    return (
-      <div key={expenseCategoryId} className="flex justify-between">
-        <div>{name}</div>
-        <div className={!description ? "invisible" : ""}>{description}</div>
-        <div>
-          Total spent per category:
-          <span>{totalSum > 0 ? separateCents(totalSum) : totalSum}</span>
-        </div>
-        {/* Delete button */}
-        <button
-          onClick={() => handleConfirmDelete(expenseCategoryId, name)}
-          className="flex"
-        >
-          &#9249;&#10062;
-        </button>
-      </div>
-    );
-  });
-
-  return <div>{categoriesJSX}</div>;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Total Spent</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {categories.map((cat) => {
+          const { expenseCategoryId, name, description, totalSum } = cat;
+          return (
+            <tr key={expenseCategoryId}>
+              <td>{name}</td>
+              <td>{description || <span>N/A</span>}</td>
+              <td>{totalSum > 0 ? separateCents(totalSum) : totalSum}</td>
+              <td>
+                {/* Delete button */}
+                <button
+                  onClick={() => handleConfirmDelete(expenseCategoryId, name)}
+                >
+                  &#9249;&#10062;
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
