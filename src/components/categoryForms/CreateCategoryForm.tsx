f6c6f8c8844792cb4bbcb4ff5ddf3e76df3f3cae
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-09 14:11:51 -0500

    feat: implement create category logic and navigate to categoriesPage upon successful creation

--- src/components/categoryForms/CreateCategoryForm.tsx
+++ src/components/categoryForms/CreateCategoryForm.tsx
@@ -1,23 +1,45 @@
+import { useNavigate } from "react-router-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
+
 import { type StringInput } from "../../types";
+import {
+  useCategoryCreateMutation,
+  createCategory,
+  useAppDispatch,
+} from "../../store";
 
 export const CreateCategoryForm = () => {
+  const [categoryCreate, { isSuccess, error }] = useCategoryCreateMutation();
   const {
     handleSubmit,
     register,
     formState: { errors },
   } = useForm<StringInput>();
 
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
   const onSubmit: SubmitHandler<StringInput> = (categoryData) => {
     handleCreateCategory(categoryData);
   };
 
-  const handleCreateCategory = (categoryData: StringInput) => {
+  const handleCreateCategory = async (categoryData: StringInput) => {
     console.log(categoryData);
+    try {
+      const newCategory = await categoryCreate(categoryData).unwrap();
+      dispatch(createCategory(newCategory));
+      console.log("Created a category:");
+      console.log(newCategory);
+
+      navigate("/categories");
+    } catch (error) {
+      throw new Error(error instanceof Error ? error.message : "Unknown error");
+    }
   };
 
   return (
     <>
+      {isSuccess ? "Category created" : error && "error"}
       <h1>Create Category</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="name">Name</label>
@@ -28,14 +50,17 @@ export const CreateCategoryForm = () => {
             required: "Name is required",
             min: {
               value: 0,
-              message: "Please include a category",
+              message: "Please include a name",
             },
           })}
         />
+        {errors.name && <span>{errors.name.toString()}</span>}
+        <br />
         <label htmlFor="description">
           Description <span>(optional)</span>
         </label>
         <input id="description" type="text" {...register("description", {})} />
+        <br />
         <button type="submit">Create</button>
       </form>
     </>
