2101d7537afa42d8a3ab1560db21c246e4e0fb23
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-11 14:44:32 -0500

    feat: install tailwind and apply global styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "slate-teal": "#3a5054",
+        bg: "#e7e3da",
+        "pale-steel": "#51789e86",
+        navy: "#0f3d66",
+        focus: "#258de9",
+        sun: "#fcd46a",
+        amber: "#f8b319",
+        charcoal: "rgba(71, 68, 68)",
+      },
+      boxShadow: {
+        custom: "1px 1px 4px 1px rgba(71, 68, 68, 0.4)",
+      },
+    },
   },
   plugins: [],
 };
