86c890f7c61895e97d31212d50f479ead7a13688
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-11 14:47:40 -0500

    feat: add tailwind to UnauthorizedLayout, remove formTitle from this component

--- src/layouts/UnauthorizedLayout.tsx
+++ src/layouts/UnauthorizedLayout.tsx
@@ -3,19 +3,36 @@ import { Outlet, Link } from "react-router-dom";
 
 import { useRedirectBox } from "../hooks";
 
+import "./unauthorizedLayout.css";
+
 export const UnauthorizedLayout: React.FC = () => {
-  const { formTitle, redirectMessage, path, buttonText } = useRedirectBox();
+  const { redirectMessage, path, buttonText } = useRedirectBox();
   return (
-    <div>
-      <header>Welcome to Budgeting App</header>
-      <div>{formTitle} form</div>
+    <div className="unauth-wrapper flex flex-col min-h-[90vh] space-y-10 justify-between">
+      <header>
+        <h1 className="mb-4">
+          Budget{" "}
+          <span className="text-transparent bg-clip-text bg-gradient-to-r to-amber from-navy">
+            Smarter
+          </span>
+          .
+        </h1>
+        <h4 className="text-2xl">
+          Take control of your finances — your goals are just a plan away!
+        </h4>
+      </header>
+
       <main>
         <Outlet />
       </main>
-      <div>{redirectMessage}</div>
-      <Link to={path}>
-        <button>{buttonText}</button>
-      </Link>
+
+      {/* Redirect box */}
+      <div className="flex justify-center gap-2.5">
+        <div>{redirectMessage}</div>
+        <Link to={path} className="underline">
+          {buttonText}
+        </Link>
+      </div>
     </div>
   );
 };
