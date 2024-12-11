cc3eaea03644bd4c67987bf6f1e1a8a834b765d7
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-11 14:48:24 -0500

    feat: add tailwind to LoginForm and SignupForm; implement Button in both components

--- src/components/userForms/LoginForm.tsx
+++ src/components/userForms/LoginForm.tsx
@@ -4,8 +4,11 @@ import { useForm, SubmitHandler } from "react-hook-form";
 
 import { budgetUpdate, useLoginMutation, userLogin } from "../../store";
 import { UserLogIn } from "../../types";
+import { useRedirectBox } from "../../hooks";
+import { Button } from "../Button";
 
 export const LoginForm: React.FC = () => {
+  const { formTitle } = useRedirectBox();
   const [login, { isLoading, error }] = useLoginMutation();
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -48,36 +51,38 @@ export const LoginForm: React.FC = () => {
     <>
       {isLoading ? "loading" : error && "error"}
       <form id="loginForm" onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="email">Email:</label>
-        <input
-          id="email"
-          type="email"
-          {...register("userEmail", {
-            required: "Email is required",
-            pattern: {
-              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-              message: "Invalid email format",
-            },
-          })}
-        />
-        {errors.userEmail && <span>{errors.userEmail.message}</span>}
-        <br />
-        <label htmlFor="password">Password:</label>
-        <input
-          id="password"
-          type="password"
-          {...register("password", {
-            required: "Password is required",
-            minLength: {
-              value: 8,
-              message: "Password must be at least 8 characters long",
-            },
-          })}
-        />
-        <br />
-        {errors.password && <span>This field is required</span>}
-        <br />
-        <button type="submit">Submit</button>
+        <div className="grid">
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            type="email"
+            {...register("userEmail", {
+              required: "Email is required",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Invalid email format",
+              },
+            })}
+          />
+          {errors.userEmail && <span>{errors.userEmail.message}</span>}
+          <br />
+          <label htmlFor="password">Password:</label>
+          <input
+            id="password"
+            type="password"
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 8,
+                message: "Password must be at least 8 characters long",
+              },
+            })}
+          />
+          <br />
+          {errors.password && <span>This field is required</span>}
+          <br />
+        </div>
+        <Button buttonType="submit" buttonText={formTitle} />
       </form>
     </>
   );
--- src/components/userForms/SignupForm.tsx
+++ src/components/userForms/SignupForm.tsx
@@ -1,8 +1,11 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { UserSignUp } from "../../types";
 import { useSignupMutation } from "../../store";
+import { useRedirectBox } from "../../hooks";
+import { Button } from "../Button";
 
 export const SignupForm: React.FC = () => {
+  const { formTitle } = useRedirectBox();
   const [signup, { isSuccess, isError, error }] = useSignupMutation();
 
   const {
@@ -38,60 +41,62 @@ export const SignupForm: React.FC = () => {
       {isError && error && (
         <p className="error-message">This email address is already is use</p>
       )}
-      <label htmlFor="displayName">Display name:</label>
-      <input
-        id="displayName"
-        type="text"
-        {...register("displayName", {
-          required: "Display name is required",
-        })}
-      />
-      <br />
-      {errors.displayName && <span>{errors.displayName.message}</span>}
-      <br />
-      <label htmlFor="email">Email:</label>
-      <input
-        id="email"
-        type="email"
-        {...register("userEmail", {
-          required: "Email is required",
-          pattern: {
-            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-            message: "Invalid email format",
-          },
-        })}
-      />
-      <br />
-      {errors.userEmail && <span>{errors.userEmail.message}</span>}
-      <br />
-      <label htmlFor="password">Password:</label>
-      <input
-        id="password"
-        type="password"
-        {...register("password", {
-          required: "Password is required",
-          minLength: {
-            value: 8,
-            message: "Password must be at least 8 characters long",
-          },
-        })}
-      />
-      <br />
-      {errors.password && <span>This field is required</span>}
-      <br />
-      <label htmlFor="repeatPassword">Repeat password:</label>
-      <input
-        id="repeatPassword"
-        type="password"
-        {...register("repeatPassword", {
-          required: "Password is required",
-          validate: (value) => value === password || "Passwords do not match",
-        })}
-      />
-      <br />
-      {errors.repeatPassword && <span>{errors.repeatPassword.message}</span>}
-      <br />
-      <button type="submit">Sign up</button>
+      <div className="grid">
+        <label htmlFor="displayName">Display name:</label>
+        <input
+          id="displayName"
+          type="text"
+          {...register("displayName", {
+            required: "Display name is required",
+          })}
+        />
+        <br />
+        {errors.displayName && <span>{errors.displayName.message}</span>}
+        <br />
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          type="email"
+          {...register("userEmail", {
+            required: "Email is required",
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "Invalid email format",
+            },
+          })}
+        />
+        <br />
+        {errors.userEmail && <span>{errors.userEmail.message}</span>}
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input
+          id="password"
+          type="password"
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters long",
+            },
+          })}
+        />
+        <br />
+        {errors.password && <span>This field is required</span>}
+        <br />
+        <label htmlFor="repeatPassword">Repeat password:</label>
+        <input
+          id="repeatPassword"
+          type="password"
+          {...register("repeatPassword", {
+            required: "Password is required",
+            validate: (value) => value === password || "Passwords do not match",
+          })}
+        />
+        <br />
+        {errors.repeatPassword && <span>{errors.repeatPassword.message}</span>}
+        <br />
+      </div>
+      <Button buttonType="submit" buttonText={formTitle} />
     </form>
   );
 };
