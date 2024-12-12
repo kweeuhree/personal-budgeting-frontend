69cd3cc5b38b2d8997ca0e1e33a346865908453c
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-12 15:10:53 -0500

    feat: Button component takes in onClick and autofocus props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,25 @@
+import { MouseEventHandler } from "react";
+
 type Props = {
   buttonType: "button" | "submit";
   buttonText: string;
+  onClick: MouseEventHandler;
+  autofocus?: boolean;
 };
 
-export const Button: React.FC<Props> = ({ buttonType, buttonText }) => {
+export const Button: React.FC<Props> = ({
+  buttonType,
+  buttonText,
+  onClick,
+  autofocus,
+}) => {
   return (
-    <button type={buttonType} className="hover:text-slate-teal">
+    <button
+      autoFocus={autofocus}
+      onClick={onClick}
+      type={buttonType}
+      className="hover:text-slate-teal"
+    >
       {buttonText}
     </button>
   );
