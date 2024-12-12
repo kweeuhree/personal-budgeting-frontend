122531631e2f712a98750db8a37c053b698f0961
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-12 09:40:10 -0500

    feat: implement Chip in DefaultLayout and update the components styling

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -2,41 +2,75 @@ import React from "react";
 import { Outlet, NavLink } from "react-router-dom";
 
 import { useAppDispatch, useLogoutMutation, userLogout } from "../store";
+import { Chip } from "../components";
+
+import "./defaultLayout.css";
+
+const centerNavLinks: { [key: string]: string } = {
+  expenses: "View expenses",
+  "expenses/create": "Create expense+",
+  categories: "View categories",
+};
+
+const confirmStmt = "Are you sure you want to log out?";
 
 export const DefaultLayout: React.FC = () => {
   const [logout] = useLogoutMutation();
   const dispatch = useAppDispatch();
 
   const handleLogout = () => {
-    try {
-      logout({});
-      dispatch(userLogout());
-    } catch (error) {
-      throw new Error(error ? error.toString() : "Failed to log out the user");
+    if (confirm(confirmStmt)) {
+      try {
+        logout({});
+        dispatch(userLogout());
+      } catch (error) {
+        throw new Error(
+          error instanceof Error ? error.message : "Failed to log out the user"
+        );
+      }
     }
+    return;
   };
 
   return (
-    <div>
-      <div>
-        <nav>
-          <div className="nav-left">
-            <NavLink to="/budget">Budget</NavLink>
-          </div>
-          <div className="nav-center">
-            <NavLink to="/expenses">View expenses</NavLink>
-            <NavLink to="/expenses/create">Create expense +</NavLink>
-            <NavLink to="/categories">View categories</NavLink>
-          </div>
-          <div className="nav-right">
-            <NavLink to="/profile">Profile</NavLink>
-            <button type="button" onClick={handleLogout}>
-              Log out
-            </button>
-          </div>
-        </nav>
-      </div>
-      <main>
+    <div className="flex flex-col min-h-[97vh]">
+      <nav className="flex content-center justify-center gap-14 text-base">
+        <div className="content-center">
+          <NavLink
+            to="/budget"
+            className={({ isActive }) => (isActive ? "selected" : "unselected")}
+          >
+            Budget
+          </NavLink>
+        </div>
+        <div className="flex gap-5 content-center">
+          {Object.entries(centerNavLinks).map(([path, text]) => (
+            <NavLink
+              key={text}
+              className={({ isActive }) =>
+                `content-center ${isActive ? "selected" : "unselected"}`
+              }
+              to={`/${path}`}
+              // ensure that react router differentiates between '/expenses' and '/expenses/create'
+              end={path === "expenses"}
+            >
+              {text}
+            </NavLink>
+          ))}
+        </div>
+        <div className="flex gap-5">
+          <NavLink
+            className={({ isActive }) =>
+              `content-center ${isActive ? "selected" : "unselected"}`
+            }
+            to="/profile"
+          >
+            Profile
+          </NavLink>
+          <Chip onClick={handleLogout} text="Log out" />
+        </div>
+      </nav>
+      <main className="content-center mt-20">
         <Outlet />
       </main>
     </div>
