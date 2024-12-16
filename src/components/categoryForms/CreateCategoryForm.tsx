b6de1a0c6e647c0a25c9fe4c4733a6024b880ecc
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-16 10:33:42 -0500

    feat: wrap each input in a div for better styling; implement useHandleNavigate in CreateCategoryForm

--- src/components/categoryForms/CreateCategoryForm.tsx
+++ src/components/categoryForms/CreateCategoryForm.tsx
@@ -1,14 +1,17 @@
-import { useNavigate } from "react-router-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
 
-import { type StringInput } from "../../types";
 import {
   useCategoryCreateMutation,
   createCategory,
   useAppDispatch,
 } from "../../store";
+import { useHandleNavigate } from "../../hooks";
+import { Button } from "../Button";
+import { type StringInput } from "../../types";
+import { responsiveHeader } from "../../styles";
 
 export const CreateCategoryForm = () => {
+  const { handleNavigate } = useHandleNavigate();
   const [categoryCreate, { isSuccess, error }] = useCategoryCreateMutation();
   const {
     handleSubmit,
@@ -16,7 +19,6 @@ export const CreateCategoryForm = () => {
     formState: { errors },
   } = useForm<StringInput>();
 
-  const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
   const onSubmit: SubmitHandler<StringInput> = (categoryData) => {
@@ -31,44 +33,50 @@ export const CreateCategoryForm = () => {
       console.log("Created a category:");
       console.log(newCategory);
 
-      navigate("/categories");
+      handleNavigate("/categories");
     } catch (error) {
       throw new Error(error instanceof Error ? error.message : "Unknown error");
     }
   };
 
-  const handleNavigate = () => {
-    navigate("/categories");
-  };
-
   return (
     <>
       {isSuccess ? "Category created" : error && "error"}
-      <h1>Create Category</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          type="text"
-          {...register("name", {
-            required: "Name is required",
-            min: {
-              value: 0,
-              message: "Please include a name",
-            },
-          })}
-        />
-        {errors.name && <span>{errors.name.toString()}</span>}
-        <br />
-        <label htmlFor="description">
-          Description <span>(optional)</span>
-        </label>
-        <input id="description" type="text" {...register("description", {})} />
-        <br />
-        <button type="submit" onClick={handleNavigate}>
-          Cancel
-        </button>
-        <button type="submit">Create</button>
+      <h3 className={responsiveHeader}>Create Category</h3>
+      <form className="my-4 sm:my-10" onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid space-y-4">
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            type="text"
+            {...register("name", {
+              required: "Name is required",
+              min: {
+                value: 0,
+                message: "Please include a name",
+              },
+            })}
+          />
+          {errors.name && <span>{errors.name.toString()}</span>}
+          <br />
+          <label htmlFor="description">
+            Description <span>(optional)</span>
+          </label>
+          <input
+            id="description"
+            type="text"
+            {...register("description", {})}
+          />
+          <br />
+        </div>
+        <div className="flex items-center justify-around">
+          <Button
+            buttonType="button"
+            buttonText="Cancel"
+            onClick={() => handleNavigate("/categories")}
+          />
+          <Button buttonType="submit" buttonText="Create" />
+        </div>
       </form>
     </>
   );
