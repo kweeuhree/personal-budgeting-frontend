7785e1fdae8fd4f42213e179e438df9457c13ded
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-16 10:38:04 -0500

    fix: wrap each input and relevant label into a div for better styling

--- src/components/AccountFieldset.tsx
+++ src/components/AccountFieldset.tsx
@@ -19,30 +19,38 @@ export const AccountFieldset: React.FC<AccountFieldsetProps> = ({
   register,
   errors,
 }) => (
-  <fieldset>
-    <legend>Account</legend>
+  <fieldset className="bg-bg p-1 border rounded-md">
+    <legend className="bg-bg px-4 border rounded-md text-md font-medium">
+      Account
+    </legend>
 
-    <input
-      id={CHECKING_BALANCE}
-      type="radio"
-      value={CHECKING_BALANCE}
-      checked={balanceType ? balanceType === CHECKING_BALANCE : undefined}
-      {...register(name, {
-        required: "Select an account type",
-      })}
-    />
-    <label htmlFor={CHECKING_BALANCE}>Checking</label>
+    <div className="flex items-center justify-around">
+      <div className="flex items-center">
+        <input
+          id={CHECKING_BALANCE}
+          type="radio"
+          value={CHECKING_BALANCE}
+          checked={balanceType ? balanceType === CHECKING_BALANCE : undefined}
+          {...register(name, {
+            required: "Select an account type",
+          })}
+        />
+        <label htmlFor={CHECKING_BALANCE}>Checking</label>
+      </div>
 
-    <input
-      id={SAVINGS_BALANCE}
-      type="radio"
-      value={SAVINGS_BALANCE}
-      checked={balanceType ? balanceType === SAVINGS_BALANCE : undefined}
-      {...register(name, {
-        required: "Select an account type",
-      })}
-    />
-    <label htmlFor={SAVINGS_BALANCE}>Savings</label>
+      <div className="flex items-center">
+        <input
+          id={SAVINGS_BALANCE}
+          type="radio"
+          value={SAVINGS_BALANCE}
+          checked={balanceType ? balanceType === SAVINGS_BALANCE : undefined}
+          {...register(name, {
+            required: "Select an account type",
+          })}
+        />
+        <label htmlFor={SAVINGS_BALANCE}>Savings</label>
+      </div>
+    </div>
 
     {errors.balanceType && <span>{errors.balanceType.message as string}</span>}
   </fieldset>
