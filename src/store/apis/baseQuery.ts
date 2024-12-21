9f54db0440882309279e06a2fc279bcdbf59ded4
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-21 08:59:04 -0500

    add logs

--- src/store/apis/baseQuery.ts
+++ src/store/apis/baseQuery.ts
@@ -3,6 +3,15 @@ import { RootState } from "..";
 
 export const baseUrl = import.meta.env.VITE_BASE_URL;
 
+const getCookie = (name: string): string | null => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(";").shift() || null;
+  return null;
+};
+
+const sessionCookie = getCookie("session"); // Extract CSRF token from the cookie
+
 // fetch csrf token during log in and store in csrf slice
 export const baseQueryWithCsrf = fetchBaseQuery({
   baseUrl: `${baseUrl}/api/`,
@@ -10,11 +19,14 @@ export const baseQueryWithCsrf = fetchBaseQuery({
   prepareHeaders: (headers, { getState }) => {
     const state = getState() as RootState;
     const csrfToken = state.csrf.token; // Get CSRF token from the Redux state
-    console.log("CSRF Token from Redux state:", state.csrf.token);
+
     if (csrfToken) {
       headers.set("X-CSRF-Token", csrfToken);
     }
     headers.set("Content-Type", "application/json");
+
+    if (sessionCookie?.length) headers.set("session", sessionCookie);
+
     console.log("Sending request with following headers:");
     headers.forEach((value, key) => {
       console.log(`${key}: ${value}`);
