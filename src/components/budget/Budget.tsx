a1c2a795122f77ca6da3ef9bbeb734f0f8876b3f
Nika Kolesnikova <firstnamenika@gmail.com>
2025-01-17 08:31:02 -0500

    feat: update Budget component: - abstract checking and savings display into Balance function; - implement Tooltip and useTooltip.

--- src/components/budget/Budget.tsx
+++ src/components/budget/Budget.tsx
@@ -4,14 +4,60 @@ import { SAVINGS_BALANCE, CHECKING_BALANCE } from "../../utils";
 import { pencilSquare } from "../../styles";
 
 import "./budget.css";
+import { Tooltip } from "../Tooltip";
+import { useTooltip } from "../../hooks";
 
 type Props = {
   handleUpdateBalance: (balanceType: string) => void;
   budget: PartialBudget;
 };
 
+type BalanceProps = {
+  balanceName: string;
+  balanceAmount?: number;
+  constant: string;
+};
+
 export const Budget: React.FC<Props> = ({ handleUpdateBalance, budget }) => {
+  const { isVisible, showTooltip, removeTooltip } = useTooltip({
+    savings: false,
+    checking: false,
+  });
   const { checkingBalance, savingsBalance, budgetTotal, totalSpent } = budget;
+
+  const Balance: React.FC<BalanceProps> = ({
+    balanceName,
+    balanceAmount,
+    constant,
+  }) => {
+    const formattedName =
+      balanceName.charAt(0).toUpperCase() + balanceName.slice(1);
+    return (
+      <div className="flex w-full justify-around py-6">
+        <div className="balance w-10/12">
+          <div>{formattedName} balance</div>
+          <div>{balanceAmount}</div>
+        </div>
+        <div className="w-1/12 flex content-center justify-center">
+          <div
+            data-tooltip-target={`tooltip-${balanceName}`}
+            className="cursor-pointer relative content-center justify-center"
+            onMouseLeave={() => removeTooltip(balanceName)}
+            onClick={() => handleUpdateBalance(constant)}
+          >
+            <div onMouseEnter={() => showTooltip(balanceName)}>
+              {pencilSquare}
+            </div>
+            <Tooltip
+              id={`tooltip-${balanceName}`}
+              text={`Update ${balanceName} balance`}
+              isVisible={isVisible[balanceName]}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div className="sm:w-5/12 md:w-7/12 lg:w-7/12 bg-white border rounded-md shadow-md mx-2 px-2 py-6 lg:mx-10 lg:py-10">
@@ -19,35 +65,17 @@ export const Budget: React.FC<Props> = ({ handleUpdateBalance, budget }) => {
       {/* Budget accounts and totals */}
       <div>
         {/* Checking balance */}
-        <div className="flex w-full justify-around py-6">
-          <div className="balance w-10/12">
-            <div>Checking balance</div>
-            <div>{checkingBalance}</div>
-          </div>
-          <div className="w-1/12 flex content-center justify-center">
-            <div
-              className="cursor-pointer content-center justify-center"
-              onClick={() => handleUpdateBalance(CHECKING_BALANCE)}
-            >
-              {pencilSquare}
-            </div>
-          </div>
-        </div>
+        <Balance
+          balanceName="checking"
+          balanceAmount={checkingBalance}
+          constant={CHECKING_BALANCE}
+        />
         {/* Savings balance */}
-        <div className="flex w-full justify-around">
-          <div className="balance w-10/12">
-            <div>Savings balance</div>
-            <div>{savingsBalance}</div>
-          </div>
-          <div className="w-1/12 flex content-center justify-center">
-            <div
-              className="cursor-pointer content-center justify-center"
-              onClick={() => handleUpdateBalance(SAVINGS_BALANCE)}
-            >
-              {pencilSquare}
-            </div>
-          </div>
-        </div>
+        <Balance
+          balanceName="savings"
+          balanceAmount={savingsBalance}
+          constant={SAVINGS_BALANCE}
+        />
         {/* Totals */}
         <div className="flex justify-evenly content-center px-2 pt-6">
           <div className="grid total">
