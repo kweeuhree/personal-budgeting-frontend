302b6ed5c52b4208ac8ce12d009e45d309649ede
Nika Kolesnikova <firstnamenika@gmail.com>
2025-01-21 09:24:27 -0500

    fix: remove commented out logic

--- src/components/CategoriesChart.tsx
+++ src/components/CategoriesChart.tsx
@@ -3,8 +3,8 @@ import ApexCharts from "apexcharts";
 
 import { selectAllExpenses, selectCategories, useAppSelector } from "../store";
 import { getChartOptions, SAVINGS_BALANCE, CHECKING_BALANCE } from "../utils";
-import { useGroupedExpenses, useHandleNavigate } from "../hooks";
-import { Button, Loading } from ".";
+import { useGroupedExpenses, useHandleNavigate, useTooltip } from "../hooks";
+import { Button, Loading, Tooltip } from ".";
 
 interface RadioInput {
   id: string;
@@ -28,23 +28,24 @@ const radioInputs: RadioInputs = {
   },
 };
 
-export const CategoriesChart = () => {
+export const CategoriesChart: React.FC = () => {
   const { handleNavigate } = useHandleNavigate();
+  const { isVisible, showTooltip, removeTooltip } = useTooltip({});
   const donutRef = useRef<HTMLDivElement | null>(null);
   const expenses = useAppSelector(selectAllExpenses);
   const categories = useAppSelector(selectCategories);
   const { groupedSavings, groupedChecking } = useGroupedExpenses();
   const [selectedAccount, setSelectedAccount] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  // const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const totalSums = categories.map((cat) => cat.totalSum);
 
   useEffect(() => {
     if (donutRef.current && categories) {
-      setIsLoading(true);
+      // setIsLoading(true);
       const chartOptions = getChartOptions(categories);
       const chart = new ApexCharts(donutRef.current, chartOptions);
-      setIsLoading(false);
+      // setIsLoading(false);
       chart.render();
 
       const updateChart = () => {
@@ -84,32 +85,31 @@ export const CategoriesChart = () => {
     setSelectedAccount((prev) => (prev === value ? null : value));
   };
 
-  const Chart = () => {
+  const Chart: React.FC = () => {
     return (
       <div className="md:w-5/12 lg:w-5/12 sm:w-full flex flex-col items-center">
-        {isLoading ? (
-          <Loading />
-        ) : (
-          <>
-            <div className="flex justify-center space-x-3">
-              {Object.entries(radioInputs).map(([label, { id, value }]) => (
-                <div key={id + "radio"} className="flex items-center space-x-1">
-                  <input
-                    id={id}
-                    type="radio"
-                    name="accountType"
-                    value={value}
-                    checked={selectedAccount === value}
-                    onChange={handleChange}
-                    className="w-4 h-4 border-amber accent-navy checked:bg-sun hover:bg-amber focus:ring-2 focus:ring-amber checked:hover:border-sun"
-                  />
-                  <label htmlFor={id + "radio"}>{label}</label>
-                </div>
-              ))}
+        <div className="flex justify-center space-x-3">
+          {Object.entries(radioInputs).map(([label, { id, value }]) => (
+            <div key={id + "radio"} className="flex items-center space-x-1">
+              <input
+                id={id}
+                type="radio"
+                name="accountType"
+                value={value}
+                checked={selectedAccount === value}
+                onChange={handleChange}
+                className="w-4 h-4 border-amber accent-navy checked:bg-sun hover:bg-amber focus:ring-2 focus:ring-amber checked:hover:border-sun"
+              />
+              <label htmlFor={id + "radio"}>{label}</label>
+              <Tooltip
+                id={`tooltip-${id}`}
+                text={`View ${id} expenses`}
+                isVisible={isVisible[id]}
+              />
             </div>
-            <div className="min-w-full" id="donut" ref={donutRef}></div>
-          </>
-        )}
+          ))}
+        </div>
+        <div className="min-w-full" id="donut" ref={donutRef}></div>
       </div>
     );
   };
